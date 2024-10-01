b5b034feb43bc695e544c7f3a513e93914a791d1
ISABELLE DA SILVA MARTINS <isabelle.smartins3@senacsp.edu.br>
2024-09-30 22:19:07 -0300

    Médias das notas do aluno, aula 30/09/2024

--- TI46ProjetosConsole/Program.cs
+++ TI46ProjetosConsole/Program.cs
@@ -10,8 +10,26 @@ namespace TI46ProjetosConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!!");
-            Console.WriteLine("Linha 2,teste");
+            string nome;
+            double nota1, nota2, nota3, nota4, resultado;
+            Console.WriteLine("Escreva seu nome");
+            nome= Console.ReadLine();
+            Console.WriteLine("Seja bem vindo "+nome);
+
+            Console.WriteLine("Digite as notas do aluno ");
+           
+            nota1 = double.Parse(Console.ReadLine());
+            nota2 = double.Parse(Console.ReadLine());
+            nota3 = double.Parse(Console.ReadLine());
+            nota4 = double.Parse(Console.ReadLine());
+
+            resultado = (nota1 + nota2 + nota3 + nota4) / 4;
+
+            Console.WriteLine("A média das notas é: " + resultado);
+           
+
+
+            Console.ReadKey();
 
         }
     }
